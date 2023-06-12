40e3998ba54b8620fdf55b1bb9c198c3a166041d
tjdhg456 <hogili89@gist.ac.kr>
2023-06-12 04:16:44 +0000

    --

--- scripts/train_student_coco.sh
+++ scripts/train_student_coco.sh
@@ -1,7 +1,7 @@
 # !/usr/bin/env bash
 
-# TOAST 
-for MODEL_NAME in faster_rcnn_x101_32x4d_fpn_1x_fskd faster_rcnn_x101_32x4d_fpn_1x_mstrain_fskd faster_rcnn_x101_32x4d_fpn_2x_fskd faster_rcnn_x101_32x4d_fpn_2x_mstrain_fskd faster_rcnn_x101_32x4d_fpn_3x_fskd faster_rcnn_x101_32x4d_fpn_3x_mstrain_fskd
+# GIST_1
+for MODEL_NAME in faster_rcnn_x101_64x4d_fpn_1x_fskd faster_rcnn_x101_64x4d_fpn_2x_fskd faster_rcnn_x101_64x4d_fpn_3x_fskd
 do
     CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
                                         --nproc_per_node=4 \
@@ -14,8 +14,8 @@ do
 done
 
 
-# GIST_1 (R50)
-for MODEL_NAME in faster_rcnn_r50_c4_1x_fskd faster_rcnn_r50_c4_1x_mstrain_fskd faster_rcnn_r50_dc5_1x_fskd faster_rcnn_r50_dc5_1x_mstrain_fskd faster_rcnn_r50_fpn_1x_fskd faster_rcnn_r50_fpn_1x_mstrain_fskd faster_rcnn_r50_fpn_2x_fskd faster_rcnn_r50_fpn_2x_mstrain_fskd faster_rcnn_r50_fpn_3x_fskd faster_rcnn_r50_fpn_3x_mstrain_fskd
+# GIST_2
+for MODEL_NAME in faster_rcnn_x101_64x4d_fpn_1x_mstrain_fskd faster_rcnn_x101_64x4d_fpn_2x_mstrain_fskd faster_rcnn_x101_64x4d_fpn_3x_mstrain_fskd
 do
     CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
                                         --nproc_per_node=4 \
@@ -26,22 +26,3 @@ do
                                         --work-dir result/coco/$MODEL_NAME \
                                         --launcher pytorch
 done
-
-
-
-
-
-# GIST_2 (R101)
-for MODEL_NAME in faster_rcnn_r101_fpn_1x_fskd faster_rcnn_r101_fpn_1x_mstrain_fskd faster_rcnn_r101_fpn_2x_fskd faster_rcnn_r101_fpn_2x_mstrain_fskd faster_rcnn_r101_fpn_3x_fskd faster_rcnn_r101_fpn_3x_mstrain_fskd 
-do
-    CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
-                                        --nproc_per_node=4 \
-                                        --master_port=199 \
-                                        train.py \
-                                        --config configs/faster_rcnn_kd/coco_$MODEL_NAME.py \
-                                        --seed 0 \
-                                        --work-dir result/coco/$MODEL_NAME \
-                                        --launcher pytorch
-done
-
-
