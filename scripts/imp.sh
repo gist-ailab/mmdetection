27dbfde8af45788f91296666e4cf6ad87fe22566
tjdhg456 <hogili89@gist.ac.kr>
2023-06-28 11:45:12 +0000

    --

--- scripts/imp.sh
+++ scripts/imp.sh
@@ -10,3 +10,16 @@ do
                                         --work-dir result/coco/mcdet/teacher/$MODEL_NAME \
                                         --launcher pytorch
 done
+
+
+for MODEL_NAME in faster_rcnn_r50_fpn_1x_0.9_0.9,0.6
+do
+    CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
+                                        --nproc_per_node=4 \
+                                        --master_port=911 \
+                                        train.py \
+                                        --config configs/faster_rcnn_mcdet_student/coco_$MODEL_NAME.py \
+                                        --seed 0 \
+                                        --work-dir result/coco/mcdet/student/$MODEL_NAME \
+                                        --launcher pytorch
+done
