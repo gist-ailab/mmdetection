2e26d8066e9c452e513e82c797cac7a12ba17405
tjdhg456 <hogili89@gist.ac.kr>
2023-06-28 11:47:59 +0000

    --

--- scripts/train_student_coco.sh
+++ scripts/train_student_coco.sh
@@ -3,7 +3,7 @@ for MODEL_NAME in faster_rcnn_r50_fpn_1x_ori_1.0,0.6
 do
     CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch \
                                         --nproc_per_node=4 \
-                                        --master_port=911 \
+                                        --master_port=928 \
                                         train.py \
                                         --config configs/faster_rcnn_mcdet_student/coco_$MODEL_NAME.py \
                                         --seed 0 \
