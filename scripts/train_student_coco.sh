fd96b35a5daacec053f250cb5e4206f405b863b9
tjdhg456 <hogili89@gist.ac.kr>
2023-07-05 08:44:51 +0000

    --

--- scripts/train_student_coco.sh
+++ scripts/train_student_coco.sh
@@ -1,9 +1,9 @@
 # !/usr/bin/env bash
-for MODEL_NAME in faster_rcnn_r101_fpn_2x_mstrain_r50_fpn_2x_0.9_0.9,0.6 faster_rcnn_r50_fpn_2x_0.9_0.9,0.6_naive
+for MODEL_NAME in faster_rcnn_r50_fpn_1x_mstrain_0.9_0.9,0.6
 do
     CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
                                         --nproc_per_node=4 \
-                                        --master_port=928 \
+                                        --master_port=888 \
                                         train.py \
                                         --config configs/faster_rcnn_mcdet_student/coco_$MODEL_NAME.py \
                                         --seed 0 \
