ad97ea9820a6bddb3f86df5bf7bc6ba961f26003
tjdhg456 <hogili89@gist.ac.kr>
2023-07-10 19:17:31 +0000

    --

--- scripts/train_teacher_coco.sh
+++ scripts/train_teacher_coco.sh
@@ -26,16 +26,3 @@ do
                                         --work-dir result/coco/LR_2x/$MODEL_NAME \
                                         --launcher pytorch
 done
-
-
-for MODEL_NAME in faster_rcnn_r101_fpn_2x_teacher
-do
-    CUDA_VISIBLE_DEVICES=3,4,5,6 python -m torch.distributed.launch \
-                                        --nproc_per_node=4 \
-                                        --master_port=310 \
-                                        train.py \
-                                        --config others/MSAD/configs/faster_rcnn_LR_msad/coco_$MODEL_NAME.py \
-                                        --seed 0 \
-                                        --work-dir others/MSAD/result/coco/LR_2x/$MODEL_NAME \
-                                        --launcher pytorch
-done
