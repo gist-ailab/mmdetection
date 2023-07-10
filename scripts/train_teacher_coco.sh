ce3620be85cff0e5537264a91f8c2b5806c9c122
tjdhg456 <hogili89@gist.ac.kr>
2023-07-10 10:11:23 +0000

    --

--- scripts/train_teacher_coco.sh
+++ scripts/train_teacher_coco.sh
@@ -1,14 +1,14 @@
 # !/usr/bin/env bash
-for MODEL_NAME in faster_rcnn_r50_fpn_1x_crop0.9_mstrain
+for MODEL_NAME in faster_rcnn_r50_fpn_2x_crop0.9_mstrain
 do
-    python -m torch.distributed.launch \
-            --nproc_per_node=8 \
-            --master_port=369 \
-            train.py \
-            --config configs/faster_rcnn_mcdet_teacher/coco_$MODEL_NAME.py \
-            --seed 0 \
-            --work-dir result/coco/mcdet/teacher/$MODEL_NAME \
-            --launcher pytorch
+    CUDA_VISIBLE_DEVICES=2,3,4,5 python -m torch.distributed.launch \
+                                        --nproc_per_node=4 \
+                                        --master_port=252 \
+                                        train.py \
+                                        --config configs/faster_rcnn_mcdet_teacher/coco_$MODEL_NAME.py \
+                                        --seed 0 \
+                                        --work-dir result/coco/mcdet/teacher/$MODEL_NAME \
+                                        --launcher pytorch
 done
 
 
